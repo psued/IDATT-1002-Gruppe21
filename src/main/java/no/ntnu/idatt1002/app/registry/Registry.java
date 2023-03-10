c9f8ab92a54076d68dd0917dce4d3bdeff26c102
Trygve Jørgensen <trygjor@stud.ntnu.no>
2023-03-10 12:06:22 +0100

    Added registry methods

--- src/main/java/no/ntnu/idatt1002/app/registry/Registry.java
+++ src/main/java/no/ntnu/idatt1002/app/registry/Registry.java
@@ -1,4 +1,33 @@
 package no.ntnu.idatt1002.app.registry;
 
+import java.util.HashMap;
+
+/**
+ * The registry interface tracks all data that is created in the application. Different
+ * registries will track different types of data.
+ */
 public interface Registry {
+  
+  /**
+   * Add an object to the registry.
+   *
+   * @param key    an immutable attribute of the object
+   * @param object the object to be added
+   */
+  void addToRegistry(Object key, Object object);
+  
+  /**
+   * Get an object from the registry.
+   *
+   * @param key the key of the object
+   * @return the specific object from the registry
+   */
+  Object getFromRegistry(Object key);
+  
+  /**
+   * Gets the entire registry.
+   *
+   * @return the entire registry
+   */
+  HashMap<Object, Object> getRegistry();
 }

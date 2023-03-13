121711459ee4958d0d26aed2d586165cb207c622
Trygve Jørgensen <trygjor@stud.ntnu.no>
2023-03-13 11:23:47 +0100

    Added Bookkeeping class

---
+++ src/main/java/no/ntnu/idatt1002/app/data/Bookkeeping.java
@@ -0,0 +1,52 @@
+package no.ntnu.idatt1002.app.data;
+
+import java.util.ArrayList;
+
+/**
+ * Bookkeeping interface is used to keep track of all the income and expenses for either
+ * budgeting for accounting.
+ */
+public interface Bookkeeping {
+  
+  /**
+   * Method for adding an income to the income list
+   *
+   * @param income the income to be added
+   */
+  void addIncome(Income income);
+  
+  /**
+   * Method for adding an expense to the expense list
+   *
+   * @param expense the expense to be added
+   */
+  void addExpense(Expense expense);
+  
+  /**
+   * Method for getting the income list
+   *
+   * @return the income list
+   */
+  ArrayList<Income> getIncomeList();
+  
+  /**
+   * Method for getting the expense list
+   *
+   * @return the expense list
+   */
+  ArrayList<Expense> getExpenseList();
+  
+  /**
+   * Method for getting the total income
+   *
+   * @return the total income amount
+   */
+  double getTotalIncome();
+  
+  /**
+   * Method for getting the total expense
+   *
+   * @return the total expense amount
+   */
+  double getTotalExpense();
+}

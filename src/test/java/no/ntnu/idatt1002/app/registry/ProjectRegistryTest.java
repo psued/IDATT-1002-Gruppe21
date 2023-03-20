d5a4fbea6250629aee0e2ac21b33a4e3f6af58c8
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2023-03-20 11:59:24 +0100

    Implemented unit tests for ProjectRegistry

--- src/test/java/no/ntnu/idatt1002/app/registry/ProjectRegistryTest.java
+++ src/test/java/no/ntnu/idatt1002/app/registry/ProjectRegistryTest.java
@@ -1,30 +1,66 @@
 package no.ntnu.idatt1002.app.registry;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import no.ntnu.idatt1002.app.data.Project;
+
 public class ProjectRegistryTest {
+
+    private ProjectRegistry projectRegistry;
+
+    @BeforeEach
+    void setUp() {
+        projectRegistry = new ProjectRegistry();
+    }
+
     @Test
     void testAddCategory() {
-
+        projectRegistry.addCategory("test");
+        ArrayList<String> categories = new ArrayList<>(projectRegistry.getCategories());
+        assertEquals("test", categories.get(2));
     }
 
     @Test
     void testAddProject() {
-
+        Project project = new Project("name", "description", "category");
+        projectRegistry.addProject(project);
+        assertEquals(project, projectRegistry.getProject(project.getCreationDate()));
     }
 
     @Test
     void testGetCategories() {
-
+        ArrayList<String> categories = new ArrayList<>(projectRegistry.getCategories());
+        assertEquals("Freelance", categories.get(0));
+        assertEquals("Personal", categories.get(1));
     }
 
     @Test
     void testGetProject() {
-
+        Project project = new Project("name", "description", "category");
+        projectRegistry.addProject(project);
+        assertEquals(project, projectRegistry.getProject(project.getCreationDate()));
     }
 
     @Test
     void testGetProjects() {
+        Project project1 = new Project("name", "description", "category");
+        Project project2 = new Project("name2", "description2", "category2");
+
+        projectRegistry.addProject(project1);
+        projectRegistry.addProject(project2);
+
+        ArrayList<Project> projects = new ArrayList<>(projectRegistry.getProjects());
+        assertEquals(2, projects.size());
+        assertTrue(projects.contains(project1));
+        assertTrue(projects.contains(project2));
 
     }
 }

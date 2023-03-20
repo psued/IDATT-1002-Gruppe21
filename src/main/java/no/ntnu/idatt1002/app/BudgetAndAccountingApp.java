fa4de7f168cef904b6f0ff1c8c743b6cb2822578
Trygve Jørgensen <trygjor@stud.ntnu.no>
2023-03-20 14:10:13 +0100

    Primitive functionality for GUI with GUI controller

--- src/main/java/no/ntnu/idatt1002/app/BudgetAndAccountingApp.java
+++ src/main/java/no/ntnu/idatt1002/app/BudgetAndAccountingApp.java
@@ -1,6 +1,10 @@
 package no.ntnu.idatt1002.app;
 
+import java.io.IOException;
+import java.util.Objects;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
@@ -14,10 +18,12 @@ public class BudgetAndAccountingApp extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent root =  FXMLLoader.load(Objects.requireNonNull(getClass().getResource("NewProject" +
+            ".fxml")));
         Region sceneRoot = new ScreenBuilder(new GUIController(), () -> {
         }).build();
-        Scene scene = new Scene(sceneRoot);
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         scene.getStylesheets().add("/css/default.css");
         primaryStage.show();
--- src/main/java/no/ntnu/idatt1002/app/gui/NewProjectController.java
+++ src/main/java/no/ntnu/idatt1002/app/gui/NewProjectController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import no.ntnu.idatt1002.app.data.Project;
 
 public class NewProjectController {
   
@@ -27,7 +26,8 @@ public class NewProjectController {
   @FXML
   private Button delete;
   
-  public Project saveProject() {
-    return new Project(name.getText(), description.getText(), category.getText());
+  @FXML
+  public void saveProject() {
+    description.setText("Project saved");
   }
 }

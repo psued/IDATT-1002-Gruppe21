23d1adc9be34037a8e7ad9fb39045b144b097b9d
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2023-03-22 11:03:20 +0100

    Started working on the User class

--- src/main/java/no/ntnu/idatt1002/app/data/User.java
+++ src/main/java/no/ntnu/idatt1002/app/data/User.java
@@ -3,13 +3,31 @@ package no.ntnu.idatt1002.app.data;
 import no.ntnu.idatt1002.app.registry.ProjectRegistry;
 
 public class User {
+
   private ProjectRegistry projectRegistry = new ProjectRegistry();
   
   public ProjectRegistry getProjectRegistry() {
     return projectRegistry;
   }
   
-  public void addProject(Project project) {
-    projectRegistry.addProject(project);
+  public void addProject(String name, String description, String category) {
+    projectRegistry.addProject(new Project(name, description, category));
   }
+
+  public void removeProject() {
+    // TODO(ingar): Needs some parameter that can get the corresponding
+    // project
+  }
+
+  public void saveToFile() {
+    // TODO(ingar): implement with classes made by Lars
+  }
+
+  public void loadFromFile() {
+    // TODO(ingar): implement with classes made by Lars
+    // Return type should be 
+  }
+
+
+
 }

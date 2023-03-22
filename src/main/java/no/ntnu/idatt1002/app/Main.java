da0d9a40e02af26eb01074c94e8bf8b16c273d70
lmlnilse <lmlnilse@stud.ntnu.no>
2023-03-22 12:41:33 +0100

    Add functionality for storing a projectRegistry.

--- src/main/java/no/ntnu/idatt1002/app/Main.java
+++ src/main/java/no/ntnu/idatt1002/app/Main.java
@@ -1,7 +1,19 @@
 package no.ntnu.idatt1002.app;
 
+import no.ntnu.idatt1002.app.fileHandling.FileHandling;
+import no.ntnu.idatt1002.app.registry.ProjectRegistry;
+
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+    ProjectRegistry projectRegistry = new ProjectRegistry();
+    System.out.println(projectRegistry.toString());
+    FileHandling.writeProjectRegistryToFile(projectRegistry);
+    ProjectRegistry pr2 = FileHandling.readProjectRegistryFromFile();
+    System.out.println(pr2.toString());
+
     BudgetAndAccountingApp.main(args);
   }
 }

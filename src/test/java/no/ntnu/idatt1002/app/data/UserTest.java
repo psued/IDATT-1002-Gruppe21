290c7fa64f80244391628a920a94f30441757759
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2023-03-23 09:52:42 +0100

    Created unit test class

---
+++ src/test/java/no/ntnu/idatt1002/app/data/UserTest.java
@@ -0,0 +1,33 @@
+package no.ntnu.idatt1002.app.data;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    
+    @Test
+    void testAddProject() {
+
+    }
+
+    @Test
+    void testEditProject() {
+
+    }
+
+    @Test
+    void testFinalize() {
+
+    }
+
+    @Test
+    void testGetProjectRegistry() {
+
+    }
+
+    @Test
+    void testRemoveProject() {
+
+    }
+
+}

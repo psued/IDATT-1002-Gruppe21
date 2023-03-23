9e6cfecda63c36be3fa06c2372e475e73227ef0a
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2023-03-23 09:56:19 +0100

    Added unit test for FileHandling

---
+++ src/test/java/no/ntnu/idatt1002/app/fileHandling/FileHandlingTest.java
@@ -0,0 +1,26 @@
+package no.ntnu.idatt1002.app.fileHandling;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import no.ntnu.idatt1002.app.data.User;
+
+public class FileHandlingTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+`    user = new User();
+    }
+
+    @Test
+    void testReadUserFromFile() {
+
+    }
+
+    @Test
+    void testWriteUserToFile() {
+
+    }
+}

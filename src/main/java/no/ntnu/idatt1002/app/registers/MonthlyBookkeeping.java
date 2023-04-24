3303efb1dc607a78a1032a1d4cd4310e5967d794
Trygve Jørgensen <trygjor@stud.ntnu.no>
2023-04-24 11:18:41 +0200

    Refactored for deep copy functionality for MonthlyBookkeeping

--- src/main/java/no/ntnu/idatt1002/app/registers/MonthlyBookkeeping.java
+++ src/main/java/no/ntnu/idatt1002/app/registers/MonthlyBookkeeping.java
@@ -7,6 +7,13 @@ import no.ntnu.idatt1002.app.bookkeeping.Accounting;
 import no.ntnu.idatt1002.app.bookkeeping.Bookkeeping;
 import no.ntnu.idatt1002.app.bookkeeping.Budgeting;
 
+/**
+ * The MonthlyBookkeeping is a register for all the bookkeeping in a month.
+ *
+ * <p>It separates the bookkeeping into two main categories and two subcategories. A transaction
+ * is either meant for the accounting or the budgeting for a month. A transaction then either put
+ * in the personal or work bookkeeping depending on what category it belongs to.
+ */
 public class MonthlyBookkeeping implements Serializable {
   
   private final Budgeting budgetingPersonal;
@@ -15,39 +22,103 @@ public class MonthlyBookkeeping implements Serializable {
   private final Accounting accountingWork;
   private final YearMonth yearMonth;
   
+  /**
+   * Creates a MonthlyBookkeeping object with empty bookkeeping objects. The yearMonth must have
+   * a valid value as it is used to identify the bookkeeping.
+   *
+   * @param yearMonth the year and month of the bookkeeping
+   * @throws IllegalArgumentException if yearMonth is null
+   */
   public MonthlyBookkeeping(YearMonth yearMonth) {
     budgetingPersonal = new Budgeting();
     budgetingWork = new Budgeting();
     accountingPersonal = new Accounting();
     accountingWork = new Accounting();
     
+    if (yearMonth == null) {
+      throw new IllegalArgumentException("YearMonth cannot be null");
+    }
     this.yearMonth = yearMonth;
   }
   
+  /**
+   * Creates a deep copy of a MonthlyBookkeeping object.
+   *
+   * @param monthlyBookkeeping the MonthlyBookkeeping object to copy
+   * @throws IllegalArgumentException if monthlyBookkeeping is null
+   */
+  public MonthlyBookkeeping(MonthlyBookkeeping monthlyBookkeeping) {
+    if (monthlyBookkeeping == null) {
+      throw new IllegalArgumentException("MonthlyBookkeeping cannot be null");
+    }
+    budgetingPersonal = new Budgeting((Budgeting) monthlyBookkeeping.getBookkeeping(false, true));
+    budgetingWork = new Budgeting((Budgeting) monthlyBookkeeping.getBookkeeping(false, false));
+    accountingPersonal = new Accounting((Accounting) monthlyBookkeeping.getBookkeeping(true, true));
+    accountingWork = new Accounting((Accounting) monthlyBookkeeping.getBookkeeping(true, false));
+    
+    yearMonth = monthlyBookkeeping.getYearMonth();
+  }
+  
+  /**
+   * Get the chosen bookkeeping object based on the parameters.
+   *
+   * <p>Does not return a deep copy as this would make it impossible to modify the bookkeeping
+   * objects.
+   *
+   * @param isAccounting true if accounting, false if budgeting
+   * @param isPersonal true if personal, false if work
+   * @return the chosen bookkeeping object
+   */
   public Bookkeeping getBookkeeping(boolean isAccounting, boolean isPersonal) {
     return isAccounting ?
         (isPersonal ? accountingPersonal : accountingWork) :
         (isPersonal ? budgetingPersonal : budgetingWork);
   }
   
+  /**
+   * Get a deep copy bookkeeping object with all the transactions of both work and personal from
+   * either accounting or budgeting.
+   *
+   * @param isAccounting true if accounting, false if budgeting
+   * @return a deep copy bookkeeping object with all the transactions of both work and personal
+   */
   public Bookkeeping getTotalBookkeeping(boolean isAccounting) {
     if (isAccounting) {
-      Accounting totalAccounting = new Accounting();
-      Stream.concat(accountingPersonal.getTransactions().stream(),
-          accountingWork.getTransactions().stream()).forEach(totalAccounting::addTransaction);
-      return totalAccounting;
+      Accounting personalCopy = new Accounting(accountingPersonal);
+      Accounting workCopy = new Accounting(accountingWork);
+      
+      Accounting totalCopy = new Accounting();
+      Stream.concat(personalCopy.getTransactions().stream(), workCopy.getTransactions().stream())
+          .forEach(totalCopy::addTransaction);
+      
+      return totalCopy;
     } else {
-      Budgeting totalBudgeting = new Budgeting();
-      Stream.concat(budgetingPersonal.getTransactions().stream(),
-          budgetingWork.getTransactions().stream()).forEach(totalBudgeting::addTransaction);
-      return totalBudgeting;
+      Budgeting personalCopy = new Budgeting(budgetingPersonal);
+      Budgeting workCopy = new Budgeting(budgetingWork);
+      
+      Budgeting totalCopy = new Budgeting();
+      Stream.concat(personalCopy.getTransactions().stream(), workCopy.getTransactions().stream())
+          .forEach(totalCopy::addTransaction);
+      
+      return totalCopy;
     }
   }
   
+  /**
+   * Get a deep copy of the yearMonth of the bookkeeping.
+   *
+   * @return a deep copy of the yearMonth of the bookkeeping
+   */
   public YearMonth getYearMonth() {
-    return yearMonth;
+    return YearMonth.from(yearMonth);
   }
   
+  /**
+   * Check if this month has any transactions by checking all the transactions in every bookkeeping
+   * object.
+   *
+   * @return true if there are no transactions in any bookkeeping object, false otherwise
+   */
   public boolean isEmpty() {
     return budgetingPersonal.getTransactions().isEmpty() &&
         budgetingWork.getTransactions().isEmpty() &&
@@ -55,20 +126,23 @@ public class MonthlyBookkeeping implements Serializable {
         accountingWork.getTransactions().isEmpty();
   }
   
+  /**
+   * Equals method for MonthlyBookkeeping. If the parameter is a MonthlyBookkeeping object but
+   * does not have the same reference, it will check if the yearMonth of the two objects are the
+   * same
+   *
+   * @param o the object to compare to
+   * @return  true if the objects are the same or if the yearMonth of the two objects are the same,
+   *          false otherwise
+   */
   @Override
   public boolean equals(Object o) {
     if (o == this) {
       return true;
     }
-    if (!(o instanceof MonthlyBookkeeping)) {
+    if (!(o instanceof MonthlyBookkeeping monthlyBookkeeping)) {
       return false;
     }
-    MonthlyBookkeeping monthlyBookkeeping = (MonthlyBookkeeping) o;
     return yearMonth.equals(monthlyBookkeeping.getYearMonth());
-  }
-  
-  @Override
-  public int hashCode() {
-    return yearMonth.hashCode();
   }
 }

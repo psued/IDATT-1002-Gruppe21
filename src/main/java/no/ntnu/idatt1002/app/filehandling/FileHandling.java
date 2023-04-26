0467deb53d18bdb78c04863cf6263f083264941e
Trygve Jørgensen <trygjor@stud.ntnu.no>
2023-04-26 17:49:16 +0200

    Jar file can now save and load data on windows

--- src/main/java/no/ntnu/idatt1002/app/filehandling/FileHandling.java
+++ src/main/java/no/ntnu/idatt1002/app/filehandling/FileHandling.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
 import no.ntnu.idatt1002.app.User;
 
 /**
@@ -14,40 +15,46 @@ import no.ntnu.idatt1002.app.User;
  */
 public class FileHandling {
   
-  private static final String file =
-      System.getProperty("user.dir") + "/src/main/resources/user.ser";
+  private static final String FILE_NAME = "user.ser";
+  private static final String DATA_DIR = System.getProperty("user.home") + File.separator +
+      ".budgetingandaccountingappdata";
+  private static final String FILE_PATH = DATA_DIR + File.separator + FILE_NAME;
   
   /**
-   * Writes a given User object to a file. The file is located in the "src/main/resources"
-   * directory and is named "user.ser".
+   * Writes a User object to a file.
    *
    * @param user The User object to be written to the file.
    * @throws IOException If an error occurs during the file writing process.
    */
   public static void writeUserToFile(User user) throws IOException {
-    FileOutputStream fileOutputStream = new FileOutputStream(file);
+    File dataDir = new File(DATA_DIR);
+    if (!dataDir.exists() && !dataDir.mkdirs()) {
+      throw new IOException("Failed to create the data directory");
+    }
+    
+    FileOutputStream fileOutputStream = new FileOutputStream(FILE_PATH);
     ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
     
     objectOutputStream.writeObject(user);
     objectOutputStream.flush();
     objectOutputStream.close();
   }
-  
+
   /**
    * Reads a User object from a file. The file is located in the "src/main/resources"
    * directory and is named "user.ser".
    *
    * @return A User object read from the file.
-   * @throws IOException            If an error occurs during the file reading process.
+   * @throws IOException If an error occurs during the file reading process.
    * @throws ClassNotFoundException If the serialized User class is not found.
    */
   public static User readUserFromFile() throws IOException, ClassNotFoundException {
-    FileInputStream fileInputStream = new FileInputStream(file);
+    FileInputStream fileInputStream = new FileInputStream(FILE_PATH);
     ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-    
+
     User user = (User) objectInputStream.readObject();
     objectInputStream.close();
-    
+
     return user;
   }
 }
